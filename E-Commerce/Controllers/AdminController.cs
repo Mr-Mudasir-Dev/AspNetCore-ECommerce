8dc8fcae0df6c7655d7e31e5c0195c645f3281d6
Mr-Mudasir-Dev <mudasiraptech@gmail.com>
2026-01-11 08:01:25 -0800

    Admin profile update implemented, needs polishing

--- E-Commerce/Controllers/AdminController.cs
+++ E-Commerce/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using E_Commerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Commerce.Controllers
 {
@@ -7,11 +8,14 @@ namespace E_Commerce.Controllers
     {
         // Global Variable But Access In AdminController
         private MyDbContext db;
+        // wwwroot Folder Access In AdminController 
+        private IWebHostEnvironment env;
 
-        // MyDbContext Access in AdminController Constructor
-        public AdminController(MyDbContext _db)
+        // MyDbContext Access in AdminController Constructor & W
+        public AdminController(MyDbContext _db, IWebHostEnvironment _env)
         {
             db = _db;
+            env = _env;
         }
 
 
@@ -20,6 +24,7 @@ namespace E_Commerce.Controllers
         {
             if (HttpContext.Session.GetInt32("Admin_id") != null)
             {
+                ViewBag.Admin_id = HttpContext.Session.GetInt32("Admin_id");
                 return View();
             }
             else
@@ -49,5 +54,86 @@ namespace E_Commerce.Controllers
             return View();
         }
 
+        // Admin Logout 
+        public IActionResult Logout()
+        {
+            if(HttpContext.Session.GetInt32("Admin_id") != null)
+            {
+                HttpContext.Session.Remove("Admin_id");
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        // Profile Page
+        public IActionResult Profile(int id)
+        {
+            if (HttpContext.Session.GetInt32("Admin_id") != null)
+            {
+                var a = db.Admins.Find(id);
+                return View(a);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult Profile(Admin admin, IFormFile Image, int id, string Crunt_Pass, string New_Pass)
+        {
+            var olddata = db.Admins.AsNoTracking().FirstOrDefault(a => a.Admin_Id == id);
+
+            if (Image != null && Image.Length > 0)
+            { 
+                var filename = Path.GetFileName(Image.FileName);
+                var filepath = Path.Combine(env.WebRootPath, "admin/admin_profile_image", filename);
+                using(var fs = new FileStream(filepath, FileMode.Create))
+                {
+                    Image.CopyTo(fs);
+                    admin.Admin_Image = filename;
+                }
+            }else
+            {
+                if(olddata.Admin_Image != null)
+                {
+                    admin.Admin_Image = olddata.Admin_Image;
+                }
+            }
+
+
+            if(!string.IsNullOrEmpty(New_Pass))
+            {
+                if (!string.IsNullOrEmpty(Crunt_Pass) && Crunt_Pass == olddata.Admin_Password)
+                {
+                    if(New_Pass != olddata.Admin_Password)
+                    {
+                        admin.Admin_Password = New_Pass;
+                        TempData["Success"] = "Your password has been successfully updated";
+                    }
+                    else
+                    {
+                        TempData["NewPass=oldpass"] = "New password must be different from current password";
+                        return RedirectToAction("Profile");
+                    }
+                }
+                else
+                {
+                    TempData["Crunt_Pass"] = "Current password is incorrect";
+                    return RedirectToAction("Profile");
+                }
+            }
+            else
+            {
+                admin.Admin_Password = olddata.Admin_Password;
+            }
+
+
+            admin.Admin_Id = id;
+            db.Admins.Update(admin);
+            db.SaveChanges();
+            TempData["msg"] = "Profile updated successfully";
+            return RedirectToAction("Profile");
+        }
     }
 }
